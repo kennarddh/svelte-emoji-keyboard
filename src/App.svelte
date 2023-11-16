649e6fc95ce8b4c2e375e7d6b4b2780a0a00f9bc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-16 21:13:43 +0700

    feat: Selection range

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,27 @@
 
   let text=''
   let index=-1
+  let inputElement:HTMLInputElement
 
-  $: console.log({text})
-  $: console.log({index})
+  $: {
+    console.log({index})
+
+    if (index === -1) {
+			inputElement?.setSelectionRange(
+				inputElement.value.length,
+				inputElement.value.length
+			)
+		}else{
+      inputElement?.setSelectionRange(index, index)
+    }
+  }
 
   const OnChange=(event:Event & { currentTarget: EventTarget & HTMLInputElement })=>{
     text = event.currentTarget?.value.replaceAll(
 			/(:[^:]+:)/gi,
 			name => {
-        console.log({name})
-				index = event.currentTarget?.value.indexOf(name)
+				index = event.currentTarget?.value.indexOf(name)+1
+        console.log({val:event.currentTarget?.value,name,index})
 
 				const emojiName = name.slice(1, -1).toLowerCase()
 
@@ -27,7 +38,7 @@
 </script>
 
 <main>
-  <input type="text" value={text} on:input={OnChange}>
+  <input type="text" value={text} on:input={OnChange} bind:this={inputElement}>
 </main>
 
 <style>
